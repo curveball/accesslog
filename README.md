5aeb0a88dc2ece1985f3e7b649698f3bb899ecc3
Evert Pot <me@evertpot.com>
2020-02-04 12:42:57 -0500

    New accesslog curveball package

--- README.md
+++ README.md
@@ -1,22 +1,48 @@
-Curveball New Package
+Curveball Access Log
 =====================
 
-This repository serves as the skeleton for all new [Curveball][1] packages.
+This package is a middleware for [Curveball][1] that will automatically log
+HTTP requests and responses to the console.
+
 
 Installation
 ------------
 
-    npm install @curveball/new-package 
+    npm install @curveball/accesslog
 
 
 Getting started
 ---------------
 
-...
+```typescript
+import accessLog from '@curveball/accesslog';
+import { Application } from '@curveball/core';
 
-API
----
+const app = new Application();
+app.use(accessLog());
+```
 
-...
+### Blacklisting urls
+
+If you'd like to remove certain urls from the access log, you can specify a
+whitelist.
+
+The default whitelist contains one url: `/health`, as this is often an
+endpoint used by load balancers and container orchestrators to see if the
+service is alive.
+
+To specify an alternative blacklist, just pass a list of strings to the
+`accessLog` function.
+
+```typescript
+import accessLog from '@curveball/accesslog';
+import { Application } from '@curveball/core';
+
+const app = new Application();
+app.use(accessLog([
+  '/ignore'
+]));
+```
+
 
 [1]: https://github.com/curveball/
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,67 @@
-describe('this package', () => {
-  it('should have tests', () => {
+import { Application } from '@curveball/core';
+import accessLog from '../src/index';
+import { expect } from 'chai';
+
+describe('accesslog', () => {
+
+  let oldConsole:any;
+  let out: any[] = [];
+
+  beforeEach( () => { 
+
+    oldConsole = console.log;
+    console.log = (...parts:any) => out.push(parts);
+    out = [];
+
+  });
+
+  afterEach( () => {
+
+    console.log = oldConsole;
+    out = [];
+
+  });
+
+  it('should log HTTP requests and responses', async () => {
+
+    const app = new Application();
+    app.use(accessLog());
+
+    app.use( ctx => {
+
+      ctx.status = 200;
+      ctx.response.body = 'hello world';
+
+    });
+
+    const response = await app.subRequest('GET', '/');
+    expect(response.status).to.equal(200);
+
+    // Rewrite the only variable: timing of the response
+    out[1][3] = '0ms';
+
+    expect(out).to.eql([
+      ['=> (%i) %s %s', 1, 'GET', '/'],
+      ['<= (%i) %s %s', 1, 200, '0ms']
+    ]);
+
+  });
+  it('should ignore things in the whitelist', async () => {
+
+    const app = new Application();
+    app.use(accessLog(['/foo']));
+
+    app.use( ctx => {
+
+      ctx.status = 200;
+      ctx.response.body = 'hello world';
+
+    });
+
+    const response = await app.subRequest('GET', '/foo');
+    expect(response.status).to.equal(200);
+
+    expect(out).to.eql([]);
 
   });
 });
